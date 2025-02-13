fa130cd0e4d8d96ea52a1a0666c4cdbbbb681759
salo <salomontilla@outlook.com>
2025-02-12 19:02:19 -0500

    funcion combinarListas terminada

--- src/EjerciciosListas.java
+++ src/EjerciciosListas.java
@@ -7,14 +7,17 @@ public class EjerciciosListas {
         //Ejercicio 1
         System.out.println("Ejercicio 1: ------");
         List<Integer> listaPotencia = List.of(2, 3, 4);
+        System.out.println("Lista original: " + listaPotencia);
         List<Integer> resultadoListaPotencia = listaPotencia(2, listaPotencia);
         System.out.println("Resultado de la potencia: " + resultadoListaPotencia);
-
 
         //Ejercicio 2
         System.out.println("Ejercicio 2: ------");
         List<Integer> listaMulti = List.of(2, 4, 6);
         List<Integer> listaMulti2 = List.of(3, 5, 7);
+
+        System.out.println("Lista 1: " + listaMulti);
+        System.out.println("Lista 2: " + listaMulti2);
 
         List<Integer> resultadoMultiplicacion = listasMultiplicacion(listaMulti, listaMulti2);
         System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
@@ -22,13 +25,16 @@ public class EjerciciosListas {
         //Ejercicio 3 - Salomon Montilla
         System.out.println("Ejercicio 3: ------");
         List<Integer> listaPromedio = List.of(2, 4, 6, 8, 10);
+        System.out.println("Lista original: " + listaPromedio);
         double resultadoPromedio = calcularPromedio(listaPromedio);
         System.out.println("Resultado del promedio: " + resultadoPromedio);
 
         //Ejercicio 4, 5, 6, 7 - Salomon Montilla
         List<Integer> lista = List.of(3, 1, 4, 1, 5, 9, 2);
         List<Integer> lista2 = List.of(10, 20, 30);
-        System.out.println("Lista original: " + lista);
+        System.out.println("Lista 1 original: " + lista);
+        System.out.println("Lista 2 original: " + lista2);
+        
         System.out.println("Ejercicio 4: ------");
         System.out.println("Lista invertida: " + invertirLista(lista));
         System.out.println("Ejercicio 5: ------");
@@ -108,8 +114,17 @@ public class EjerciciosListas {
         }
         return true;
     }
+    //ejercicio 7
+    private static List<Integer> combinarListas(List<Integer> lista, List<Integer> lista2) {
+        List<Integer> listaCombinada = new ArrayList<>();
+        for (int num : lista) {
+            listaCombinada.add(num);
+        }
 
-    private static String combinarListas(List<Integer> lista, List<Integer> lista2) {
+        for (int num : lista2) {
+            listaCombinada.add(num);
+        }
+        return listaCombinada;
     }
 
 
